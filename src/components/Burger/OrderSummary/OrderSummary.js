731c270a3d066cad65c7f2d0dbd7abd488c3f6b7
Achigurupati <amarnath.chigurupati@eurecom.fr>
2019-09-03 10:53:09 +0200

    burger builder: adding ingredients, removing ingredients, order summary and responsive design

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Aux from '../../../hoc/Aux/Aux';
+import Button from '../../UI/Button/Button';
+
+class OrderSummary extends Component {
+    componentDidUpdate(){
+        console.log('[OrderSummary will update]')
+    }
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+        .map(ingKey => {
+            return ( <li key={ingKey}>
+                        <span style={{textTransform: 'capitalize'}}>{ingKey}</span>: {this.props.ingredients[ingKey]}
+                    </li> );
+        });
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A declicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price} </strong></p>
+                <p>Continue to Checkout..</p>
+                <Button btnType = "Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+                <Button btnType = "Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
+        );
+    }
+}
+export default OrderSummary;
